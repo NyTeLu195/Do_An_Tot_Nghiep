541f6e83b7ad6b2f9ddaf0ceb8f7b9cc1a9c4ecb
NyTeLu195 <84112772+NyTeLu195@users.noreply.github.com>
2022-06-17 07:37:20 +0700

    Add project files.

---
+++ Do_An_Tot_Nghiep/Models/Classroom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Do_An_Tot_Nghiep.Models
+{
+    public class Classroom
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+        
+        public Guid HomeroomTeacherID { get; set; }
+
+        public int TotalNumberStudent { get; set; }
+        public int Grade { get; set; }
+
+        public string Status { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdate { get; set; }
+
+        public Guid UserCreataID { get; set; }
+
+        public Guid UserUpdateID { get; set; }
+
+
+    }
+}
---
+++ Do_An_Tot_Nghiep/Models/Teacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Do_An_Tot_Nghiep.Models
+{
+    public class Teacher
+    {
+        public Guid Id { get; set; }
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Status { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdate { get; set; }
+
+        public Guid UserCreataID { get; set; }
+
+        public Guid UserUpdateID { get; set; }
+
+    }
+}
